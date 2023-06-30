db2e111da4d5e5affc060358d65ea9addac8374c
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-30 10:41:14 +0200

    Adding items to cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -9,7 +9,7 @@ const cartSlice = createSlice({
 	reducers: {
 		addItemToCart(state, action) {
 			const newItem = action.payload;
-			const existingItem = state.item.find(item => item.id === newItem.id);
+			const existingItem = state.items.find(item => item.id === newItem.id);
 			if (!existingItem) {
 				state.items.push({
 					itemId: newItem.id,
@@ -24,17 +24,17 @@ const cartSlice = createSlice({
 			}
 		},
 		removeItemFromCart(state, action) {
-      const id = action.payload;
-      const existingItem = state.items.find(item=>item.id===id);
-      if(existingItem.quantity===1){
-        state.items=state.items.filter(item=>item.id!==id)
-      }else{
-        existingItem.quantity--;
-        existingItem.totalPrice=existingItem.totalPrice-existingItem.price;
-      }
-    },
+			const id = action.payload;
+			const existingItem = state.items.find(item => item.id === id);
+			if (existingItem.quantity === 1) {
+				state.items = state.items.filter(item => item.id !== id);
+			} else {
+				existingItem.quantity--;
+				existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+			}
+		},
 	},
 });
 
-export const cartActions =cartSlice.actions;
+export const cartActions = cartSlice.actions;
 export default cartSlice;
