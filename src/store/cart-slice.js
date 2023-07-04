5baf8391a2e48b8aced5dff5bd0f64bcafe2c427
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-04 17:54:07 +0200

    Fetching data

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
 	name: "cart",
@@ -39,50 +38,7 @@ const cartSlice = createSlice({
 	},
 });
 
-export const sendCartData = cart => {
-	return async dispatch => {
-		dispatch(
-			uiActions.showNotification({
-				status: "pending",
-				title: "Sending...",
-				message: "Sending Cart data",
-			})
-		);
 
-		 const sendRequest = async () => {
-			const response = await fetch(
-				"https://training-2d545-default-rtdb.firebaseio.com/cart.json",
-				{
-					method: "PUT",
-					body: JSON.stringify(cart),
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error("Sendind cart data failed");
-			}
-		};
-
-		try {
-			await sendRequest();
-			dispatch(
-				uiActions.showNotification({
-					status: "success",
-					title: "Success!",
-					message: "Sent cart data successfully!",
-				})
-			);
-		} catch (error) {
-			dispatch(
-				uiActions.showNotification({
-					status: "error",
-					title: "Error!",
-					message: "Sending cart data failed!",
-				})
-			);
-		}
-	};
-};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
