56e7b9c9346a82f6b707de4523bcf428091b36b8
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-04 18:10:06 +0200

    Finalizing fetch data

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -17,7 +17,10 @@ export const fetchCartData = () => {
 		};
 		try {
 			const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+			dispatch(cartActions.replaceCart({
+        items:cartData.items || [],
+        totalQuantity:cartData.totalQuantity,
+      }));
 		} catch (error) {
 			dispatch(
 				uiActions.showNotification({
@@ -45,7 +48,10 @@ export const sendCartData = cart => {
 				"https://training-2d545-default-rtdb.firebaseio.com/cart.json",
 				{
 					method: "PUT",
-					body: JSON.stringify(cart),
+					body: JSON.stringify({
+						items: cart.items,
+						totalQuantity: StaticRange.totalQuantity,
+					}),
 				}
 			);
 
